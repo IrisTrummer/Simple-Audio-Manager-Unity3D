3f663f26e7312db293bd1eeb5d38e3138fa80030
Iris Trummer <iris.trummer@aon.at>
2024-01-01 10:27:11 +0100

    Raise event when method field component is interacted with

--- Audio Manager/Assets/Scripts/Testing/Fields/Field.cs
+++ Audio Manager/Assets/Scripts/Testing/Fields/Field.cs
@@ -1,20 +1,45 @@
+using System;
 using Core;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Testing.Fields
 {
-    public class Field : MonoBehaviour
+    public class Field : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField]
         private TMP_Text fieldNameText;
 
         public string FieldName { get; private set; }
-        
+
+        public Action Interact;
+
         public void SetFieldName(string fieldName)
         {
             FieldName = fieldName;
             fieldNameText.SetTextBetweenTags(fieldName);
+        }
+
+        public void SetActive(bool active)
+        {
+        }
+
+        // interface callbacks, does not work for some child components like text fields
+        public void OnPointerClick(PointerEventData _)
+        {
+            OnPointerClick();
+        }
+
+        // invokable from EventTrigger component through the inspector
+        public void OnPointerClick()
+        {
+            RaiseInteractEvent();
+        }
+
+        private void RaiseInteractEvent()
+        {
+            Interact.Invoke();
         }
     }
 }
